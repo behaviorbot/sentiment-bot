a26c99b9f7c25376c67a2ea8ed86ef4b7f1e8238
hiimbex <hiimbex@github.com>
2017-07-12 10:39:06 -0700

    added error hanlding and yml file - waiting on CoC API access

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-var googleapis = require('googleapis');
+const googleapis = require('googleapis');
+const yaml = require('js-yaml');
 
 module.exports = robot => {
-    robot.on('issues', check);
     robot.on('issue_comment', check);
-    robot.on('pull_request', check);
     async function check(context) {
-        //This is used to compare to the data we get from the Perspective API
-        const toxicity_low_threshold = .5;
-        const toxicity_high_threshold = .8;
-
-        robot.log(context.payload);
-        let context_issue = context.payload.issue || context.payload.pull_request;
-        if (!context.payload.issue) {
-            context_issue = (await context.github.issues.get(context.issue())).data;
-        }
-
-        if (context.payload.comment) {
-            //robot.log("this is actually a comment!");
-            var body = context.payload.comment.body;
-        } else {
-            var body = context_issue.body;
-        }
         
-        robot.log(body);
+        robot.log(context.github.repos);
+        let config, toxicityThreshold;
+        //This is used to compare to the data we get from the Perspective API
+        try {
+            const options = context.repo({path: '.github/sentiment-bot.yml'});
+            const response = await context.github.repos.getContent(options);
+            config = yaml.load(Buffer.from(response.data.content, 'base64').toString()) || {};
+        } catch (err) {
+            if (err.code !== 404) throw err;
+        }
+
+        if (config) toxicityThreshold = config.toxicityThreshold;
+        else toxicityThreshold = .7;
+
+        const body = context.payload.comment.body;
+        robot.log(body, config);
         
         //attempt to make perspective api requests template from google
         discovery_url = 'https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1'
@@ -35,20 +33,15 @@ module.exports = robot => {
             };
             client.comments.analyze({key: process.env.PERSPECTIVE_API_KEY, resource: analyze_request}, (err, response) => {
                 if (err) throw err;
-                //robot.log(JSON.stringify(response, null, 2));
                 var toxic_value = response.attributeScores.TOXICITY.spanScores[0].score.value
                 robot.log(toxic_value);
 
-                if (toxic_value >= toxicity_high_threshold) {
+                if (toxic_value >= toxicityThreshold) {
+                    // Check for Code of Conduct
+                    // Cry ??
                     robot.log("That comment was very toxic.");
-                    var template = 'That comment was very toxic!';
-                    return context.github.issues.createComment(context.issue({body: template}));
-                } else if (toxic_value >= toxicity_low_threshold) {
-                    robot.log("That comment was potentially toxic.");
-                    var template = 'Maybe consider saying something nicer next time!';
-                    return context.github.issues.createComment(context.issue({body: template}));
-                } else {
-                    robot.log("That comment wasn't very toxic.");
+                    var template = 'Hey there, that comment triggered our sentiment analysis warning. Make sure to check out this Repo\'s code of conduct!';
+                    context.github.issues.createComment(context.issue({body: template}));
                 }
             });
         });
