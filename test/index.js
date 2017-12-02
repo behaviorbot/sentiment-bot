fc2bf744b76dc7201674af7045e08a35f7a1cac8
Steven Loria <sloria1@gmail.com>
2017-12-02 08:41:23 -0500

    Use perspective-api-client and improve tests
    - Use a client library to simplify the code
    - Test integration with mocked API

--- test/index.js
+++ test/index.js
@@ -3,41 +3,74 @@ const {createRobot} = require('probot')
 const plugin = require('..')
 const payload = require('./events/payload')
 
-describe('sentiment-bot', () => {
-  let robot
-  let github
+const createTestRobot = ({toxicity}) => {
+  // PERSPECTIVE_API_KEY must be set
+  process.env.PERSPECTIVE_API_KEY = 'mock-key'
+  const robot = createRobot()
+  plugin(robot)
 
-  beforeEach(() => {
-    robot = createRobot()
-    plugin(robot)
-
-    github = {
-      repos: {
-        getContent: expect.createSpy().andReturn(Promise.resolve({
-          data: {
-            content: Buffer.from(`
-              sentimentBotToxicityThreshold: 0.3
-              sentimentBotReplyComment: "That comment was toxic"`).toString('base64')
-          }
-        })),
-        get: expect.createSpy().andReturn(Promise.resolve({
-          data: {
-            code_of_conduct: Buffer.from(`
-              name: 'Contributor Covenenant'
-              url: https://github.com/hiimbex/testing-things/blob/master/CODE_OF_CONDUCT.md`).toString('base64')
-          }
-        }))
-      },
-      issues: {
-        createComment: expect.createSpy()
-      }
+  const github = {
+    repos: {
+      getContent: expect.createSpy().andReturn(Promise.resolve({
+        data: {
+          content: Buffer.from(`
+            sentimentBotToxicityThreshold: 0.3
+            sentimentBotReplyComment: "That comment was toxic"`).toString('base64')
+        }
+      })),
+      get: expect.createSpy().andReturn(Promise.resolve({
+        data: {
+          code_of_conduct: Buffer.from(`
+            name: 'Contributor Covenenant'
+            url: https://github.com/hiimbex/testing-things/blob/master/CODE_OF_CONDUCT.md`).toString('base64')
+        }
+      }))
+    },
+    issues: {
+      createComment: expect.createSpy()
     }
+  }
 
-    robot.auth = () => Promise.resolve(github)
-  })
+  // Mock perspective API client
+  const perspective = {
+    analyze: expect.createSpy().andReturn(Promise.resolve(
+      {
+        attributeScores: {
+          TOXICITY: {
+            spanScores: [
+              {
+                begin: 0,
+                end: 56,
+                score: {
+                  value: toxicity,
+                  type: 'PROBABILITY'
+                }
+              }
+            ],
+            summaryScore: {
+              value: toxicity,
+              type: 'PROBABILITY'
+            }
+          }
+        },
+        languages: [
+          'en'
+        ]
+      }
+    ))
+  }
 
+  robot.auth = () => Promise.resolve(github)
+  robot.perspective = perspective
+  return robot
+}
+
+describe('sentiment-bot', () => {
   describe('sentiment-bot success', () => {
     it('posts a comment because the user was toxic', async () => {
+      const robot = createTestRobot({toxicity: 0.8})
+      const github = await robot.auth()
+      const perspective = robot.perspective
       await robot.receive(payload)
       expect(github.repos.getContent).toHaveBeenCalledWith({
         owner: 'hiimbex',
@@ -52,13 +85,16 @@ describe('sentiment-bot', () => {
           Accept: 'application/vnd.github.scarlet-witch-preview+json'
         }
       })
-      // Imitate google api stuff
-      // expect(github.issues.createComment).toHaveBeenCalled();
+      expect(perspective.analyze).toHaveBeenCalled()
+      expect(github.issues.createComment).toHaveBeenCalled()
     })
   })
 
   describe('sentiment-bot fail', () => {
     it('does not post a comment because the user was not toxic', async () => {
+      const robot = createTestRobot({toxicity: 0.2})
+      const github = await robot.auth()
+      const perspective = robot.perspective
       await robot.receive(payload)
 
       expect(github.repos.getContent).toHaveBeenCalledWith({
@@ -73,6 +109,7 @@ describe('sentiment-bot', () => {
           Accept: 'application/vnd.github.scarlet-witch-preview+json'
         }
       })
+      expect(perspective.analyze).toHaveBeenCalled()
       expect(github.issues.createComment).toNotHaveBeenCalled()
     })
   })
