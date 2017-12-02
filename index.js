7ef4338bfa8c82cf65ba5e4e5b2de132f5807726
Steven Loria <sloria1@gmail.com>
2017-12-02 11:22:14 -0500

    Use truncate: true to prevent error from Perspective
    Don't send text > 3000 characters, otherwise Perspective with
    return an Error

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = robot => {
         if (repoData.data.code_of_conduct) {
           codeOfConduct = Object.assign({}, repoData.data.code_of_conduct)
         }
-        const response = await robot.perspective.analyze(body)
+        const response = await robot.perspective.analyze(body, {truncate: true})
         const toxicValue = response.attributeScores.TOXICITY.spanScores[0].score.value
         // If the comment is toxic, comment the comment
         if (toxicValue >= toxicityThreshold) {
